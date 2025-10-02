d411f1ce4671ed4b6b56d3e3da6c4e0d0bb661ab
FFFusco7107 <2202655@spsd.sk.ca>
2025-10-02 13:42:01 -0600

    New demo

---
+++ 10 Image Animations/sketch.js
@@ -0,0 +1,48 @@
+// Image Animations
+// Lucas F. 
+// Oct 2, 2025 
+
+// Global Variables
+let pinImages = []; // array === list
+let current = 0; 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  loadAssets();
+  print("done loading"); 
+  // framerate(5); // usually use framerate to debug 
+
+
+}
+
+async function loadAssets(){
+  // load all of our pinwheel images
+  for(let i = 0; i < 9; i++){
+   pinImages.push(await loadImage("assets/pin-0"+i+".png")); 
+  }
+  
+}
+
+function draw() { // This IS a loop
+  background(0);
+  // animateWithFor();
+
+  // manage current image to display
+  if (frameCount % 4 === 0){
+    current += 1
+  if (current > 8) current = 0
+  }
+  imageMode(CENTER)
+  image(pinImages[current], width/2, height*0.6)
+} // Screen is upadtaed here!
+
+function animateWithFor(){
+  // Try to make an animation with a FOR loop
+  // Doesnt work! 
+  // FOR Loop yeilds a SINGLE IMAGE
+  
+  for(i = 0; i < 9; i++){
+    image(pinImages[i], width/2, height*0.6);
+    
+  }
+}
