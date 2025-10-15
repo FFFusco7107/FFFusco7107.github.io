5ef3e3bed15fdb4bda83677cd1cdae18f21f3d40
FFFusco7107 <2202655@spsd.sk.ca>
2025-10-15 13:59:55 -0600

    new demo

--- 15 Objects (Bookshelf)/sketch.js
+++ 15 Objects (Bookshelf)/sketch.js
@@ -2,14 +2,31 @@
 // Lucas F.
 // Oct 15, 2025
 
+// Global Variables SEction
+let myBook;         // for testing one
+let bookshelf = []; // for testing many 
+
+// use a loop and an array to generate 20+ skinnyish books,
+// and display them
+// 
+// "title and author can be he same for each"
+// eg   bookshelf.push( new Book("a",""))
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // create single book 
+  myBook = new Book("CS30 Text", "Mr. Scott", 
+    1234567891011, "leatherbound",
+    515, width*0.3);
+  
 }
 
 function draw() {
   background(220);
+  myBook.display();
 }
+
+
 
 
 // typically organaize class to bottom 
@@ -24,10 +41,32 @@ class Book{
     this.isbn = isbn; 
     this.x = x;
     this.pickedUp = false; 
+    // feilds for pickup
+    this.left; this.right; this.top; this.bottom;
+
   }
   
   // 2. class methods 
+  updateSides(){
+    this.top = height/2 -75;
+    this.bottom = height/2 + 75;
+    this.left = this.x - this.pages/20;
+    this.right = this.x + this.pages/20;
+  }
+
+  mouseIsOver(){
+    // return whether the mouse is hovering or not
+    if(mouseX > this.left && mouseX < this.right){
+      if(mouseY < this.bottom && mouseY > this.top){
+        return true;
+      }
+    }
+    return false; 
+  }
+
+
   display(){
+    this.updateSides();
     // render our book object on the canvas
     rectMode(CENTER);   textAlign(CENTER, CENTER);
     textSize(20); 
@@ -44,13 +83,12 @@ class Book{
     // now, draw the book elements
     push();
     translate(this.x, height/2);
+    if(this.mouseIsOver()){
+      scale(1.1);
+    }
     rect(0,0,this.pages/10,150);
     fill(255);
-    text(this.title[0], this.x, height/2 - 50);
+    text(this.title[0], 0, -50);
     pop();
-  }
-
-  printOut(){
-
   }
 }
