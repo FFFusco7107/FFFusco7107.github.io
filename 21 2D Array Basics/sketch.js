eeba28ede0f4e1306427cf897d79fa53240df938
FFFusco7107 <2202655@spsd.sk.ca>
2025-11-03 14:10:02 -0600

    new demo

---
+++ 21 2D Array Basics/sketch.js
@@ -0,0 +1,69 @@
+// 2D Array Basics
+// Lucas F.
+// Nov 3, 2025
+
+// 0 (black) 255 (white)
+
+let grid = [
+  [0, 0, 0, 255, 0],
+  [255, 0, 255, 0, 255],
+  [255, 255, 0, 255, 255],
+  [0, 255, 0, 0, 0]
+];
+
+let rows = grid.length;
+let cols = grid[0].length;
+
+let squareSize = 60;
+
+function setup() {
+  createCanvas(cols*squareSize, rows*squareSize);
+
+}
+
+function draw() {
+  background(220);
+  renderGrid();
+  print(getCurrentX(), getCurrentY());
+}
+
+function mousePressed(){
+  //flip the current tile 
+  // upgrade: only do this is mouse is on canvas
+  
+  let x = getCurrentX();
+  let y = getCurrentY();
+
+  // ALWAYS: flip the "focused" tile
+  flip(x,y);
+}
+
+function getCurrentX(){
+  // determine current col of mouse position
+  let constrainedX = constrain(mouseX, 0, width-1);
+  return floor(constrainedX / squareSize);
+}
+
+function getCurrentY(){
+  // determine current col of mouse position
+  let constrainedY = constrain(mouseY, 0, height-1);
+  return floor(constrainedY / squareSize);
+}
+
+function flip(x,y){
+  //takes a tile at x,y and inverts its value
+  if(grid[y][x] === 0) grid[y][x] = 255;
+  else grid[y][x] = 0;
+}
+
+function renderGrid(){
+  // interpret the information in the 2D array, and draw a grid of
+  // squares on the screen to reflect it
+  for(let y = 0;y < rows; y++){
+    for(let x = 0; x < cols; x++){
+      let fillColor = grid[y][x];
+      fill(fillColor);
+      square(x*squareSize, y*squareSize, squareSize);
+    }
+  }
+}
