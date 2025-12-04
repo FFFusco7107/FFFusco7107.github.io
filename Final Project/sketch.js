eb5a14325aa954b84ba92b8d35458fd9c5f430ab
FFFusco7107 <2202655@spsd.sk.ca>
2025-12-04 12:19:30 -0600

    project

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -6,14 +6,16 @@ let cube;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  cube = new Cube(width*0.2,height*0.7, 45);
 }
 
 function draw() {
   background(220);
   noStroke();
   fill(255,0,0);
-  cube = new Cube(width*0.2,height*0.7, 45);
+  
   cube.display();
+  cube.move();
    
   // ground
   fill(0);
@@ -22,7 +24,8 @@ function draw() {
 
 function keyPressed(){
   if (keyCode === UP_ARROW){
-     cube.move();
+    // rotate(framecount * 0.5);
+     cube.jump();
      console.log("pressed");
   }
 }
@@ -30,23 +33,30 @@ function keyPressed(){
 class Cube{
   // cube that jumps when pressing spacebar
   constructor(x,y,s){
-    this.x = x; this.y = y; 
     this.s = s;
     this.g = createVector(0,0.2);
     this.pos = createVector(x,y);
-    this.vel = createVector(10);
+    this.vel = createVector(0,0);
   }
   display(){
     fill(255,0,0);
-    square(this.x, this.y - this.s, this.s);
+    square(this.pos.x, this.pos.y - this.s, this.s);
+    fill(0,255,0);
+    square(this.pos.x + 5,this.pos.y- this.s, this.s - 30)
   }
   move(){
-
       this.vel.add(this.g);
-      this.vel.limit(100); // cant go outside -20 and 20
+      this.vel.limit(100);
       this.pos.add(this.vel);
+      if(this.pos.y > height*0.7){
+        this.pos.y = height*0.7;
+      }
 
   }
-  
+  jump(){
+    if(this.pos.y === height*0.7){
+      this.vel.y = -6;
+    }
+  }
 }
 
