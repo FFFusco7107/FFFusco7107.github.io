e2092ca596f48d4251a144c7984fbd06042e4af4
FFFusco7107 <2202655@spsd.sk.ca>
2025-12-07 13:39:00 -0600

    project

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -12,7 +12,6 @@ function setup() {
 function draw() {
   background(220);
   noStroke();
-  fill(255,0,0);
   
   cube.display();
   cube.move();
@@ -20,13 +19,11 @@ function draw() {
   // ground
   fill(0);
   rect(0, height*0.7, width, height);
-}
 
-function keyPressed(){
-  if (keyCode === UP_ARROW){
-    // rotate(framecount * 0.5);
-     cube.jump();
-     console.log("pressed");
+  // spacebar -> jump
+  if(keyIsDown(32)){
+    cube.jump();
+    console.log("pressed");
   }
 }
 
@@ -34,28 +31,55 @@ class Cube{
   // cube that jumps when pressing spacebar
   constructor(x,y,s){
     this.s = s;
-    this.g = createVector(0,0.2);
     this.pos = createVector(x,y);
+    this.g = createVector(0,0.2);
     this.vel = createVector(0,0);
+
+    this.rotation = 0;
+    this.rotationSpeed = 0;
+    this.onGround = true;
   }
   display(){
+    push();
+    translate(this.pos.x + this.s/2, this.pos.y - this.s /2);
+    rotate(radians(this.rotation));
     fill(255,0,0);
-    square(this.pos.x, this.pos.y - this.s, this.s);
+    square(-this.s /2, -this.s /2, this.s);
     fill(0,255,0);
-    square(this.pos.x + 5,this.pos.y- this.s, this.s - 30)
+    square(-this.s/2 + 5,this.s/2 - this.s, this.s - 30)
+    pop();
   }
   move(){
       this.vel.add(this.g);
       this.vel.limit(100);
       this.pos.add(this.vel);
-      if(this.pos.y > height*0.7){
+
+      // If on ground
+      if(this.pos.y >= height*0.7){
         this.pos.y = height*0.7;
+        this.vel.y = 0;
+
+        if(!this.onGround){
+          this.onGround = true;
+
+          // snaps the cube to nearest right angle (90,180,270,360...)
+          this.rotation = round(this.rotation/90) * 90;
+          this.rotationSpeed = 0;
+        }
+      }
+      // If in air -> rotate
+      else {
+        this.onGround = false;
+        this.rotation += this.rotationSpeed;
       }
 
   }
   jump(){
-    if(this.pos.y === height*0.7){
+    // can only jump when on ground
+    if(this.onGround){ 
       this.vel.y = -6;
+      this.rotationSpeed = 1.6; // rotates 1.6 degrees every frame
+      this.onGround = false;
     }
   }
 }
