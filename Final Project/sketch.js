271db77b17037af6f9dfef9341195e94ca999bff
FFFusco7107 <2202655@spsd.sk.ca>
2025-12-15 14:11:08 -0600

    cube gravity and speed

--- Final Project/sketch.js
+++ Final Project/sketch.js
@@ -5,15 +5,14 @@
 let player;
 let platform;
 let level = [];
-let levelSpeed = 9;
+let levelSpeed = 6;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   player = new Cube(width*0.2,height*0.7, 45);
   level.push(new box(500,height*0.7 - 45, 45));
   level.push(new spike(700,height*0.7));
-  level.push(new spike(745,height*0.7));
-  level.push(new spike(790,height*0.7));
+
   
 }
 
@@ -24,19 +23,18 @@ function draw() {
   player.move();
   for(let o of level){ 
     o.display();
-    o.slide();
+    o.slide();  
   }  
    
   // ground 
   fill(0);
-  rect(0, height*0.7, width, height);  
+  rect(0, height*0.7, width, height);
 
   // spacebar -> jump
   if(keyIsDown(32)){
     player.jump();
     console.log("pressed");
   }
-
    
 }
 
@@ -45,11 +43,11 @@ class Cube{
   constructor(x,y,s){
     this.s = s;
     this.pos = createVector(x,y);
-    this.g = createVector(0,0.4);
+    this.g = createVector(0,0.5);
     this.vel = createVector(0,0);
 
     this.rotation = 0;
-    this.rotationSpeed = 0;
+    this.rotationSpeed = 0; 
     this.onGround = true;
   }
   display(){
@@ -60,7 +58,7 @@ class Cube{
     fill(255,200,50);
     square(-this.s /2, -this.s /2, this.s);
     fill(0,255,255);
-    square(-this.s/2 + 9,this.s/2 - this.s + 9, this.s - 38);
+    square(-this.s/2 + 9,this.s/2 - this.s +  9, this.s - 38);
     square(-this.s/2 + 28,this.s/2 - this.s + 9, this.s - 38);
     rectMode(CENTER);
     rect(-this.s/2 + 22,this.s/2 - this.s + 30,this.s/2 + 5, this.s - 40);
@@ -88,23 +86,24 @@ class Cube{
       else {
         this.onGround = false;
         this.rotation += this.rotationSpeed;
-      }
+      } 
 
-  }
+  } 
   jump(){
     // can only jump when on ground
     if(this.onGround){ 
-      this.vel.y = -9;
-      this.rotationSpeed = 4; // rotates 1.6 degrees every frame
+      this.vel.y = -10;
+      this.rotationSpeed = 4.5; // rotates 1.6 degrees every frame
       this.onGround = false;
     }
-  }
+
+  } 
 }
 
 class box{
   constructor(x,y,s){
-    this.x = x; this.y = y; this.s = s;
-  }  
+    this.x = x; this.y = y; this.s = s; 
+  } 
 
   display(){
     push();
